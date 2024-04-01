8c6c47b81c1c032c47a1a889d6039b609fda9185
shubradip23 <94664658+shubradip23@users.noreply.github.com>
2024-04-01 15:57:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Students...')
+  res.send('Hello shubradip...')
 })
 
 app.listen(4000, ()=>{
